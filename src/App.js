5ebd0e55c700be3d8bf9dedda30702bba191ce76
Lang <chenshutong168@163.com>
2021-07-01 15:12:05 +1000

    -

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Greeting from Amplify. F-1 F-1
+          Greeting from Amplify.
         </p>
         <a
           className="App-link"
