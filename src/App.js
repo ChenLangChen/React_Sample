5496de5fc1c051e73e73ecb928a408f7bf31debe
Lang <chenshutong168@163.com>
2021-07-01 12:08:19 +1000

    Changed p

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I'm b_1 branch.
+          I'm b_1 branch. Hello!
         </p>
         <a
           className="App-link"
