d1f89739ec0ec388ba8f1831ac8673243a32c0f1
Lang <chenshutong168@163.com>
2021-07-01 11:48:55 +1000

    Changed heading again

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Greeting from b_1 branch. I wanna test the Preview :)
+          Greeting from b_1 branch. :)
         </p>
         <a
           className="App-link"
