0548fc0eb3d95b180d15be8b67eddbbbbd671c6a
Lang <chenshutong168@163.com>
2021-07-01 14:11:45 +1000

    changed heading

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I'm b_1 branch. Hello! Conflict resolved! I hope :)
+          Greeting from Amplify.
         </p>
         <a
           className="App-link"
